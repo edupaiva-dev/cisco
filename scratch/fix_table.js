a8173354f503374e6cf87170df8bdd2a07cd6386
eduardo <edupaiva.dev@gmail.com>
2026-06-27 20:35:42 -0500

    Eliminar scripts temporales

--- scratch/fix_table.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('6-1427200-4.html', 'utf8');
-c = c.replace('<table class="w-full text-left border-collapse">', '<table class="w-full text-left border-collapse block overflow-x-auto whitespace-nowrap">');
-fs.writeFileSync('6-1427200-4.html', c);
